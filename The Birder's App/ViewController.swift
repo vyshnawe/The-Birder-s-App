029d90f2f81203ae5d0f520d0e6c061166c776b2
student <s528803@nw36616.nwmissouri.edu>
2017-10-15 19:53:42 -0500

    Initial Commit

---
+++ The Birder's App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Birder's App
+//
+//  Created by Kalvakota,Vaishnavi on 10/15/17.
+//  Copyright © 2017 Kalvakota,Vaishnavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
