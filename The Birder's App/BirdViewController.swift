9637fc1fd784bdd66d1f61ccb2e322ebb68b8cb0
Kalvakota <s528803@nw42562.nwmissouri.edu>
2017-10-15 20:07:36 -0500

    First Commit and added required viewControllers with Navigation

---
+++ The Birder's App/BirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BirdViewController.swift
+//  The Birder's App
+//
+//  Created by Kalvakota,Vaishnavi on 10/15/17.
+//  Copyright © 2017 Kalvakota,Vaishnavi. All rights reserved.
+//
+
+import UIKit
+
+class BirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- The Birder's App/CountriesTableViewController.swift
+++ The Birder's App/CountriesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CountriesTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
