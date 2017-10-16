6924c7c556988d76719d977ceb757aa75d72857f
student <s528803@nw36616.nwmissouri.edu>
2017-10-15 21:11:47 -0500

    Added elements in BirdViewController and constraints to the elements

--- The Birder's App/BirdViewController.swift
+++ The Birder's App/BirdViewController.swift
@@ -9,27 +9,31 @@
 import UIKit
 
 class BirdViewController: UIViewController {
-
+    
+    var birdObj:String!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLavel: UILabel!
+    @IBOutlet weak var sightingsTextField: UITextField!
+    
+    
+    @IBAction func updateSightingsButton(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.title = birdObj
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
