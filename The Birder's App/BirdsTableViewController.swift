36354c7bb4745edaa02975d7576d5437d8ff137e
student <s528803@nw42562.nwmissouri.edu>
2017-10-15 21:07:54 -0500

    Added programmatic code for Country and Birds table view controllers for populating data.

--- The Birder's App/BirdsTableViewController.swift
+++ The Birder's App/BirdsTableViewController.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class BirdsTableViewController: UIViewController {
-
+class BirdsTableViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var countryObj:String!
+    var birds:[String]!  = ["Parrot", "Eagle", "Crane", "hen"]
+    var secondTableView: UITableView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.navigationItem.title = countryObj
+        
+        secondTableView = UITableView(frame: CGRect(x: 0, y: 10, width: self.view.frame.width, height: self.view.frame.height))
+        secondTableView.register(UITableViewCell.self, forCellReuseIdentifier: "birds_cell")
+        secondTableView.delegate = self
+        secondTableView.dataSource = self
+        view.addSubview(secondTableView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return birds.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "birds_cell", for: indexPath)
+        cell.textLabel?.text = birds[indexPath.row]
 
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Now we instantiate a CitiesFlownTableViewController, where we will display a list of cities flown
+        
+        let birdVC:BirdViewController = BirdViewController()
+        
+        // Examine CitiesFlownTableViewController -- you will see it defines an airline property that we configure here
+        // so that by the time that TVC becomes visible, the airline's property & cities will be displayed to the user
+        
+        birdVC.birdObj = birds[indexPath.row]
+        
+        // Every UIViewController, if it is part of a UINavigationController stack, has a navigationController property that references that
+        // UINavigationController.  This is quite handy, when we want to push a new view controller
+        self.navigationController?.pushViewController(birdVC, animated: true)
+    }
 }
+
--- The Birder's App/CountriesTableViewController.swift
+++ The Birder's App/CountriesTableViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class CountriesTableViewController: UIViewController {
-
+class CountriesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var countries:[String]!  = ["India", "USA", "South America", "Australia"]
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.frame = CGRect(x: 0, y: 10, width: self.view.frame.width, height: self.view.frame.height)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell_country")
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Now we instantiate a CitiesFlownTableViewController, where we will display a list of cities flown
+        
+        let birdsTVC:BirdsTableViewController = BirdsTableViewController()
+        
+        // Examine CitiesFlownTableViewController -- you will see it defines an airline property that we configure here
+        // so that by the time that TVC becomes visible, the airline's property & cities will be displayed to the user
+        
+        birdsTVC.countryObj = countries[indexPath.row]
+        
+        self.navigationController?.pushViewController(birdsTVC, animated: true)
+    }
 }
 
